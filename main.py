37478ead6c53b544022541f2b67a9a2b9bb0ffc8
Alireza Nabavieh <nabavieh@gmail.com>
2022-09-08 00:52:02 +0430

    first

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.title('Our first steam application')
+st.subheader('Introduction Streamlit')
+
+st.write('''
+This is the first web App
+''')
+
+# streamlit run main.py
